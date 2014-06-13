fc89c2497e2b224fa2f1e265b9049a0615e9da45
Dmytro Sokil <dmytro.sokil@gmail.com>
2014-06-13 15:55:34 +0300

    show method name

--- src/Sokil/Rest/Client/Response.php
+++ src/Sokil/Rest/Client/Response.php
@@ -38,7 +38,7 @@ class Response
     public function __call($name, $arguments)
     {
         if(!method_exists($this->_structure, $name)) {
-            throw new \Exception('Wrong method specified');
+            throw new \Exception('Wrong method "' . $name . '" specified');
         }
         
         $result = call_user_func_array(array($this->_structure, $name), $arguments);
